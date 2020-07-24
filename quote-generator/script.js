141a6d09e613311d3ad204fc335418307d3ee263
Freeda Moore <freedamoore@outlook.com>
2020-07-24 16:01:56 +1000

    add quote-generator code

---
+++ quote-generator/script.js
@@ -0,0 +1,75 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+let errorCount = 0;
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Get Quote from API
+async function getQuote() {
+    showLoadingSpinner();
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+
+        //if Author is blank, add 'Unknown' instead
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        //Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        //Stop Loader and show the quote
+        removeLoadingSpinner();
+    } catch (error) {
+        //some quotes have weird characters that throw exceptions, 
+        //so run getQuote() to get next one if error thrown
+        //as this is a recursive function, only run 5 times to avoid infinite loop
+
+        if (errorCount < 5) {
+            getQuote();
+            console.log(errorCount);
+            errorCount++;
+        } else {
+            console.log("getQuote failed more than 5 times.")
+        }
+
+    }
+}
+
+//Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+//On Load
+getQuote();
